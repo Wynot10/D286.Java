5873f15c8af68d2023582c66e23dc68eeeece06b
Justin <jwyant@wgu.org>
2026-01-07 05:40:08 -0600

    add material to lesson  2_12

--- variablesAssignments2/src/typeConversions2_12.java
+++ variablesAssignments2/src/typeConversions2_12.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class typeConversions2_12 {
     public static void main(String[] args) {
-        System.out.print("Next lesson");
+        Scanner scnr = new Scanner(System.in);
+        int numBirths = 316;
+
+        /* Implicit or automatic type cast on integer numbirths casting to double
+        since dividing by a double number (0.504)*/
+        double expectedMales = 0.504 * numBirths;
+
+        System.out.print("Expected number of males: ");
+
+        // Manipulate decimal to print 1 decimal place
+        System.out.printf("%.1f\n",  expectedMales);
     }
 }
