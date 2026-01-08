3b5deb2814f675e3c75858f7ede8aebeaf8279e1
Justin <jwyant@wgu.org>
2026-01-07 18:48:18 -0600

    finish type conversions file

--- variablesAssignments2/src/typeConversions2_12.java
+++ variablesAssignments2/src/typeConversions2_12.java
@@ -12,5 +12,21 @@ public class typeConversions2_12 {
 
         // Manipulate decimal to print 1 decimal place
         System.out.printf("%.1f\n",  expectedMales);
+
+        int kidsInFamily1;       // Should be int, not double
+        int kidsInFamily2;       // (know anyone with 2.3 kids?)
+        int numFamilies;
+
+        double avgKidsPerFamily; // Expect fraction, so double
+
+        kidsInFamily1 = 3;
+        kidsInFamily2 = 4;
+        numFamilies = 2;
+
+        // Explicit type cast, 1 type cast would have worked too.
+        avgKidsPerFamily = (double)(kidsInFamily1 + kidsInFamily2)
+                / (double)numFamilies;
+
+        System.out.println("Average kids per family: " + avgKidsPerFamily);
     }
 }
