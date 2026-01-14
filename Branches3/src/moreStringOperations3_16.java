c0ca4cf963c19ed71cf94a5c6b22da3d8e8b3b01
Justin <jwyant@wgu.org>
2026-01-13 19:51:04 -0600

    add

--- Branches3/src/moreStringOperations3_16.java
+++ Branches3/src/moreStringOperations3_16.java
@@ -4,7 +4,7 @@ public class moreStringOperations3_16 {
         Scanner scnr = new Scanner(System.in);
         String emailTxt;
         int atSymbol;
-        String userName = "";
+        String userName1 = "";
         boolean userNameIsValid = false;
         String myString = "Good bye";
 
@@ -16,16 +16,39 @@ public class moreStringOperations3_16 {
             System.out.println("Invalid email address");
         }
         else {
-            userName = emailTxt.substring(0, atSymbol);
+            userName1 = emailTxt.substring(0, atSymbol);
             userNameIsValid = true;
         }
 
         if (userNameIsValid) {
             System.out.print("Username is: ");
-            System.out.println(userName);
+            System.out.println(userName1);
         }
 
         myString = myString.replace("bye", "evening");
-        System.out.print(myString);
+        System.out.println(myString);
+
+        String userName;
+        String greetingText;
+
+        System.out.print("Enter name: ");
+        userName = scnr.nextLine();
+
+        // Combine strings using +
+        greetingText = "Hello " + userName;
+
+        // Append a period (could have used +)
+        greetingText = greetingText.concat(".");
+        System.out.println(greetingText);
+
+        // Insert Mr/Ms before user's name
+        greetingText = "Hello Mr/Ms ";
+        greetingText = greetingText.concat(userName);
+        greetingText = greetingText.concat(".");
+        System.out.println(greetingText);
+
+        // Replace occurrence of "Darn" by "@$#"
+        greetingText = greetingText.replace("Darn", "@$#");
+        System.out.println(greetingText);
     }
 }
