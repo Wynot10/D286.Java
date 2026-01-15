48af75c97eebb3962c457ab3c6cbabf12cb6c6d6
Justin <jwyant@wgu.org>
2026-01-15 17:06:43 -0600

    add lesson 4.5

---
+++ Loops4/src/moreJavaLoops4_5.java
@@ -0,0 +1,13 @@
+public class moreJavaLoops4_5 {
+    public static void main(String [] args) {
+        int currC;
+        double equivalentF;
+
+        for (currC = -10; currC <= 40; currC += 5) {
+            equivalentF = (currC * 9.0 / 5.0) + 32.0;
+
+            System.out.print(currC + " C is ");
+            System.out.println(equivalentF + " F");
+        }
+    }
+}
