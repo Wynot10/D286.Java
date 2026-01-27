e495ac175658eecc0a2fc5d7e6f69f35854d2166
Justin <jwyant@wgu.org>
2026-01-27 05:23:11 -0600

    add objects

--- objectsAndClasses7/src/Cars.java
+++ objectsAndClasses7/src/Cars.java
@@ -4,6 +4,28 @@ public class Cars {
     private String color;
     private int year;
 
+
+    // Constructor Overloading when declaring numerous constructors
+
+    // No Argument Constructor used to set default values
+    public Cars() {
+        make = "Make of Car";
+        model = "Model of Car";
+        color = "Color of Car";
+        year = -1;
+    }
+
+    // 1 Argument Constructor used to set default values
+    // You can have as many declared constructors as long
+    // as they do not have the same amount of argument lists
+    public Cars(int year) {
+        make = "Created with 4 argument constructor";
+        model = "Model of Car";
+        color = "Color of Car";
+        year = -10;
+    }
+
+
     public void setMake(String carMake) {
         make = carMake;
     }
--- objectsAndClasses7/src/MyCars.java
+++ objectsAndClasses7/src/MyCars.java
@@ -1,7 +1,10 @@
 public class MyCars {
     public static void main(String[] args) {
-        Cars car1 = new Cars();
+        Cars car1 = new Cars(); // Calling the constructor
+        Cars car2 = new Cars(2020 ); // Calling defined 1 argument constructor
 
+        System.out.println("Default Cars Values Printed ");
+        System.out.println(car1.printCars());
 
         car1.setMake("GMC");
         car1.setModel("Acadia");
@@ -9,5 +12,7 @@ public class MyCars {
         car1.setYear(2025);
 
        System.out.println(car1.printCars());
+
+       System.out.println(car2.printCars());
     }
 }
