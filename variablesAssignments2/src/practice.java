0290c7f927f34a1d19f6fce01878625e98be0cc5
Justin <jwyant@wgu.org>
2026-01-30 05:10:34 -0600

    import Houses package and create house object

--- variablesAssignments2/src/practice.java
+++ variablesAssignments2/src/practice.java
@@ -1,58 +1,13 @@
-import java.util.Scanner;
+// Import User Created House Package with Houses Object from #objectsAndClasses7
+// This practice file is located in variablesAssignments2
+import House.Houses;
 
 public class practice {
     public static void main (String[] args) {
-        char arrowBody;
-        char arrowHead;
+        Houses house1 = new Houses();
 
-        // Char variables are surrounded by single quotes ' '
-        arrowBody = '-';
-        arrowHead = '>';
-
-        System.out.println(arrowHead);
-        System.out.println("" + arrowBody + arrowBody + arrowBody + arrowHead);
-
-        arrowBody = 'o';
-
-        System.out.println("" + arrowBody + arrowBody + arrowBody + arrowHead);
+        house1.printHouse();
 
 
-
-        Scanner scnr = new Scanner(System.in);
-        char bodyChar;
-        char headChar;
-
-        System.out.print("Enter two characters: ");
-
-        /*
-        The charAt(0) is explained in another section. Briefly, next() gets the next sequence of non-whitespace
-        characters (as a string), and charAt(0) gets the first character in that string
-         */
-        bodyChar = scnr.next().charAt(0);
-        headChar = scnr.next().charAt(0);
-
-        // Output arrow body then head
-        System.out.print(bodyChar);
-        System.out.print(bodyChar);
-        System.out.print(bodyChar);
-        System.out.println(headChar);
-
-        /*
-        Escape sequence	Char
-        \n	newline
-        \t	tab
-        \'	single quote
-        \"	double quote
-        \\	backslash
-         */
-
-        char c1 = 'y';
-        char c2 = 'a';
-        /*
-        The initial "" tells the compiler to output a string of characters, and the +'s combine the subsequent
-        characters into such a string. Without the "", the compiler will simply add the numerical values of c1 and c2,
-        and output the resulting sum.
-         */
-        System.out.print("" + c1 + c2);
     }
 }
